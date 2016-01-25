221d3eff3541f7583dbfaaf81aca84a49934917a
Alex Martin <alex_martin_7@hotmail.com>
2016-01-25 14:10:59 -0500

    Update Spinal_Cord.ino

--- Spinal_Cord.ino
+++ Spinal_Cord.ino
@@ -2,8 +2,11 @@
 
 //Servo SB, SF, HB, HF, EB, EF, KB, KF; 
 Servo myservo1, myservo2, myservo3, myservo4, myservo5, myservo6, myservo7, myservo8;
+String readString, amp, knee_amp, afreq, gait, phase, f_offset, b_offset, fk_offset, bf_offset;
 
-String readString, servo1, servo2, servo3, servo4, servo5, servo6, servo7, servo8;
+unsigned long previousMillis = 0;
+unsigned int sine_time = 0.05;
+unsigned int t = 0;
 
 void setup(){
   //Start serial stream at 115200 baud
@@ -23,7 +26,6 @@ void setup(){
 }
 
 void loop(){
-  int i = 1;
   char c;
     
   while (Serial.available() && readString.length() < 33){
@@ -43,57 +45,30 @@ void loop(){
     Serial.println(readString.length()); 
     
     //Parse serial string into separate parameters
-    servo1 = readString.substring(0, 4);
-    servo2 = readString.substring(4, 8); 
-    servo3 = readString.substring(8, 12); 
-    servo4 = readString.substring(12, 16); 
-    servo5 = readString.substring(16, 20); 
-    servo6 = readString.substring(20, 24); 
-    servo7 = readString.substring(24, 28); 
-    servo8 = readString.substring(28, 32); 
+    amp = readString.substring(0, 4);
+    knee_amp = readString.substring(4, 8); 
+    afreq = readString.substring(8, 12); 
+    gait = readString.substring(12, 16); 
+    phase = readString.substring(16, 20); 
+    f_offset = readString.substring(20, 24); 
+    b_offset = readString.substring(24, 28); 
+    fk_offset = readString.substring(28, 32); 
+    bk_offset = readString.substring(32, 36);
     
     /*
     //Print to serial monitor to see parsed results
-    Serial.print(servo1);  
-    Serial.print(servo2);
-    Serial.print(servo3);  
+    Serial.print(servo1); 
+    Serial.print(servo2); 
+    Serial.print(servo3); 
     Serial.print(servo4);
-    Serial.print(servo5); 
+    Serial.print(servo5);
     Serial.print(servo6);
     Serial.print(servo7);  
     Serial.println(servo8);
     */
     
-    int n1 = servo1.toInt();
-    int n2 = servo2.toInt();
-    int n3 = servo3.toInt();
-    int n4 = servo4.toInt();
-    int n5 = servo5.toInt();
-    int n6 = servo6.toInt();
-    int n7 = servo7.toInt();
-    int n8 = servo8.toInt();
-    
-    pos[0] = int(amp) * sin(float(afreq)*t) + 1500 + int(f_offset)
-    pos[1] = int(amp) * sin(float(afreq)*t) + 1522 - int(f_offset)
-    pos[2] = int(amp) * sin(float(afreq)*t + float(gait)) + 1500 + int(b_offset)
-    pos[3] = int(amp) * sin(float(afreq)*t + float(gait)) + 1446 - iny(b_offset)
- 
-    pos[4] = int(amp)*float(knee_amp) * sin(float(afreq)*t + float(phase)) + 1500 + int(fk_offset)
-    pos[5] = int(amp)*float(knee_amp) * sin(float(afreq)*t + float(phase)) + 1581 - int(fk_offset)
-    pos[6] = int(amp)*float(knee_amp) * sin(float(afreq)*t + float(gait) + float(phase)) + 1500 + int(bk_offset)
-    pos[7] = int(amp)*float(knee_amp) * sin(float(afreq)*t + float(gait) + float(phase)) + 1538 - int(bk_offset)
-           
-    //Set servo positions
-    myservo1.writeMicroseconds(n3);
-    myservo2.writeMicroseconds(n7);
-    myservo3.writeMicroseconds(n1); 
-    myservo4.writeMicroseconds(n5);
-    myservo5.writeMicroseconds(n4); 
-    myservo6.writeMicroseconds(n8);
-    myservo7.writeMicroseconds(n2); 
-    myservo8.writeMicroseconds(n6);
-      
-    //Clear strings
+    /*
+    //clear strings
     readString="";
     servo1="";
     servo2="";
@@ -103,5 +78,37 @@ void loop(){
     servo6="";
     servo7="";
     servo8="";
+    */
+  }
+  //determine joint positions
+  pos[0] = int(amp) * sin(float(afreq)*t) + 1500 + int(f_offset)
+  pos[1] = int(amp) * sin(float(afreq)*t) + 1522 - int(f_offset)
+  pos[2] = int(amp) * sin(float(afreq)*t + float(gait)) + 1500 + int(b_offset)
+  pos[3] = int(amp) * sin(float(afreq)*t + float(gait)) + 1446 - iny(b_offset)
+ 
+  pos[4] = int(amp)*float(knee_amp) * sin(float(afreq)*t + float(phase)) + 1500 + int(fk_offset)
+  pos[5] = int(amp)*float(knee_amp) * sin(float(afreq)*t + float(phase)) + 1581 - int(fk_offset)
+  pos[6] = int(amp)*float(knee_amp) * sin(float(afreq)*t + float(gait) + float(phase)) + 1500 + int(bk_offset)
+  pos[7] = int(amp)*float(knee_amp) * sin(float(afreq)*t + float(gait) + float(phase)) + 1538 - int(bk_offset)
+  
+  //get time in milliseconds
+  unsigned long currentMillis = millis();
+  
+  //if the time threshold has been passed...
+  if(currentMillis - previousMillis >= sine_time){
+    previousMillis = currentMillis;
+    
+    //write servo positions
+    myservo1.writeMicroseconds(pos[2]);
+    myservo2.writeMicroseconds(pos[6]);
+    myservo3.writeMicroseconds(pos[0]); 
+    myservo4.writeMicroseconds(pos[4]);
+    myservo5.writeMicroseconds(pos[3]); 
+    myservo6.writeMicroseconds(pos[7]);
+    myservo7.writeMicroseconds(pos[1]); 
+    myservo8.writeMicroseconds(pos[5]);
+    
+    //increment time parameter
+    t+=1;
   }
 }

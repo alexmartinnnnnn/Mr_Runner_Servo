61b13935fa0328069034bddc890635bda7818dc7
Alex Martin <alex_martin_7@hotmail.com>
2016-01-25 00:41:56 -0500

    Update Spinal_Cord.ino

--- Spinal_Cord.ino
+++ Spinal_Cord.ino
@@ -5,9 +5,11 @@ Servo myservo1, myservo2, myservo3, myservo4, myservo5, myservo6, myservo7, myse
 
 String readString, servo1, servo2, servo3, servo4, servo5, servo6, servo7, servo8;
 
-void setup() {
+void setup(){
+  //Start serial stream at 115200 baud
   Serial.begin(115200);
   
+  //Connect servos to Arduino
   myservo1.attach(2);
   myservo2.attach(3);
   myservo3.attach(4);
@@ -20,24 +22,28 @@ void setup() {
   delay(3000);
 }
 
-void loop() {
+void loop(){
   int i = 1;
   char c;
     
-  while (Serial.available() && readString.length() < 33) {
-    delayMicroseconds(150);  //delay to allow buffer to fill
-    //delay(1);
-    if (Serial.available() > 0) {
-      char c = Serial.read();  //gets one byte from serial buffer
-      readString += c; //makes the string readString
+  while (Serial.available() && readString.length() < 33){
+    //delay to allow buffer to fill
+    delayMicroseconds(150);
+    if (Serial.available() > 0){
+      //get one byte from serial buffer
+      char c = Serial.read();
+      //creates the string "readString"
+      readString += c;
     }
   }
 
   if (readString.length() > 0){
-    Serial.println("The string is: " + readString); //see what was received
+    //See what was received
+    Serial.println("The string is: " + readString);
     Serial.println(readString.length()); 
     
-    servo1 = readString.substring(0, 4); //get the first four characters
+    //Parse serial string into separate parameters
+    servo1 = readString.substring(0, 4);
     servo2 = readString.substring(4, 8); 
     servo3 = readString.substring(8, 12); 
     servo4 = readString.substring(12, 16); 
@@ -46,8 +52,9 @@ void loop() {
     servo7 = readString.substring(24, 28); 
     servo8 = readString.substring(28, 32); 
     
-    /* 
-    Serial.print(servo1);  //print to serial monitor to see parsed results
+    /*
+    //Print to serial monitor to see parsed results
+    Serial.print(servo1);  
     Serial.print(servo2);
     Serial.print(servo3);  
     Serial.print(servo4);
@@ -66,17 +73,18 @@ void loop() {
     int n7 = servo7.toInt();
     int n8 = servo8.toInt();
     
-    pos[0] = amp * sin(afreq*t) + 1500 + f_offset
-    pos[1] = amp * sin(afreq*t) + 1522 - f_offset
-    pos[2] = amp * sin(afreq*t + gait) + 1500 + b_offset
-    pos[3] = amp * sin(afreq*t + gait) + 1446 - b_offset
+    pos[0] = int(amp) * sin(float(afreq)*t) + 1500 + int(f_offset)
+    pos[1] = int(amp) * sin(float(afreq)*t) + 1522 - int(f_offset)
+    pos[2] = int(amp) * sin(float(afreq)*t + float(gait)) + 1500 + int(b_offset)
+    pos[3] = int(amp) * sin(float(afreq)*t + float(gait)) + 1446 - iny(b_offset)
  
-    pos[4] = amp*knee_amp * sin(afreq*t + phase) + 1500 + fk_offset
-    pos[5] = amp*knee_amp * sin(afreq*t + phase) + 1581 - fk_offset
-    pos[6] = amp*knee_amp * sin(afreq*t + gait + phase) + 1500 + bk_offset
-    pos[7] = amp*knee_amp * sin(afreq*t + gait + phase) + 1538 - bk_offset
+    pos[4] = int(amp)*float(knee_amp) * sin(float(afreq)*t + float(phase)) + 1500 + int(fk_offset)
+    pos[5] = int(amp)*float(knee_amp) * sin(float(afreq)*t + float(phase)) + 1581 - int(fk_offset)
+    pos[6] = int(amp)*float(knee_amp) * sin(float(afreq)*t + float(gait) + float(phase)) + 1500 + int(bk_offset)
+    pos[7] = int(amp)*float(knee_amp) * sin(float(afreq)*t + float(gait) + float(phase)) + 1538 - int(bk_offset)
            
-    myservo1.writeMicroseconds(n3); //set servo positions
+    //Set servo positions
+    myservo1.writeMicroseconds(n3);
     myservo2.writeMicroseconds(n7);
     myservo3.writeMicroseconds(n1); 
     myservo4.writeMicroseconds(n5);
@@ -85,7 +93,8 @@ void loop() {
     myservo7.writeMicroseconds(n2); 
     myservo8.writeMicroseconds(n6);
       
-    readString=""; //clear strings
+    //Clear strings
+    readString="";
     servo1="";
     servo2="";
     servo3="";

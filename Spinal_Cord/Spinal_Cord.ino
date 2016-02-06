a33d09c18971cf1baf00d1cde390292fa1f1618b
alexmartinnnnnn <alex_martin_7@hotmail.com>
2016-02-05 23:56:08 -0500

    Now Functional

--- Spinal_Cord/Spinal_Cord.ino
+++ Spinal_Cord/Spinal_Cord.ino
@@ -1,6 +1,10 @@
 #include <Servo.h>
 
 Servo SB, SF, HB, HF, EB, EF, KB, KF;
+int iAmp, iF_offset, iB_offset, iFk_offset, iBk_offset, pos[] = {0,1,2,3,4,5,6,7};
+unsigned int t = 0;
+unsigned long previousMillis = 0;
+float fKnee_amp, fAfreq, fPhase, fGait;
 
 void setup(){
   //Start serial stream at 115200 baud
@@ -20,16 +24,13 @@ void setup(){
 }
 
 void loop(){
-  char c, buffer[10], pos[] = {0,1,2,3,4,5,6,7,8};
-  int iAmp, iF_offset, iB_offset, iFk_offset, iBk_offset;
-  float fKnee_amp, fAfreq, fPhase, fGait;
+  char c, buffer[10];
   String readString, amp, knee_amp, afreq, gait, phase, f_offset, b_offset, fk_offset, bk_offset;
-  unsigned long previousMillis = 0;
-  unsigned int sine_time = 0.05, t = 0;
+  unsigned int sine_time = 0.05;
     
-  while (Serial.available() && readString.length() < 38){
+  while (Serial.available() && readString.length() < 37){
     //delay to allow buffer to fill
-    delayMicroseconds(150);
+    delayMicroseconds(100);
     if (Serial.available() > 0){
       //get one byte from serial buffer
       char c = Serial.read();
@@ -38,7 +39,7 @@ void loop(){
     }
   }
 
-  if (readString.length() > 0){
+  if (readString.length() > 36){
     //See what was received
     Serial.println("The string is: " + readString);
     Serial.println(readString.length()); 
@@ -47,26 +48,24 @@ void loop(){
     amp = readString.substring(0, 3);
     knee_amp = readString.substring(3, 7); 
     afreq = readString.substring(7, 11); 
-    phase = readString.substring(12, 16); 
-    gait = readString.substring(16, 21); 
-    f_offset = readString.substring(21, 25); 
-    b_offset = readString.substring(25, 29); 
-    fk_offset = readString.substring(29, 33); 
-    bk_offset = readString.substring(33, 37);
-    
+    phase = readString.substring(11, 15); 
+    gait = readString.substring(15, 20); 
+    f_offset = readString.substring(20, 24); 
+    b_offset = readString.substring(24, 28); 
+    fk_offset = readString.substring(28, 32); 
+    bk_offset = readString.substring(32, 36);
     /*
     //Print to serial monitor to see parsed results
-    Serial.print(amp); 
-    Serial.print(knee_amp); 
-    Serial.print(afreq); 
-    Serial.print(gait);
-    Serial.print(phase);
-    Serial.print(f_offset);
-    Serial.print(b_offset);  
-    Serial.print(fk_offset);
-    Serial.println(bk_offset);
+    Serial.println("Hip Amp: " + amp); 
+    Serial.println("Knee Amp: " + knee_amp); 
+    Serial.println("Afreq: " + afreq); 
+    Serial.println("Gait: " + gait);
+    Serial.println("Phase: " + phase);
+    Serial.println("F Offset: " + f_offset);
+    Serial.println("B Offset: " + b_offset);  
+    Serial.println("Fk Offset: " + fk_offset);
+    Serial.println("Bk Offset: " + bk_offset + "\n");
     */
-    
     iAmp = amp.toInt();
   
     knee_amp.toCharArray(buffer, 10);
@@ -89,7 +88,7 @@ void loop(){
     iB_offset = b_offset.toInt();
     iFk_offset = fk_offset.toInt();
     iBk_offset = bk_offset.toInt();
-    
+   
     //clear strings
     readString="";
     amp="";
@@ -103,35 +102,59 @@ void loop(){
     bk_offset="";
   }
   
-  //determine joint positions
-  pos[0] = iAmp * sin(fAfreq*t) + 1500 + iF_offset;
-  pos[1] = iAmp * sin(fAfreq*t) + 1522 - iF_offset;
-  pos[2] = iAmp * sin(fAfreq*t + fGait) + 1500 + iB_offset;
-  pos[3] = iAmp * sin(fAfreq*t + fGait) + 1446 - iB_offset;
+  if (fAfreq > 0){
+    //determine joint positions
+    pos[0] = iAmp * sin(fAfreq*t) + 1500 + iF_offset;
+    pos[1] = iAmp * sin(fAfreq*t) + 1522 - iF_offset;
+    pos[2] = iAmp * sin(fAfreq*t + fGait) + 1500 + iB_offset;
+    pos[3] = iAmp * sin(fAfreq*t + fGait) + 1446 - iB_offset;
  
-  pos[4] = iAmp*fKnee_amp * sin(fAfreq*t + fPhase) + 1500 + iFk_offset;
-  pos[5] = iAmp*fKnee_amp * sin(fAfreq*t + fPhase) + 1581 - iFk_offset;
-  pos[6] = iAmp*fKnee_amp * sin(fAfreq*t + fGait + fPhase) + 1500 + iBk_offset;
-  pos[7] = iAmp*fKnee_amp * sin(fAfreq*t + fGait + fPhase) + 1538 - iBk_offset;
-  
-  //get time in milliseconds
-  unsigned long currentMillis = millis();
-  
-  //if the time threshold has been passed...
-  if(currentMillis - previousMillis >= sine_time){
-    previousMillis = currentMillis;
+    pos[4] = iAmp*fKnee_amp * sin(fAfreq*t + fPhase) + 1500 + iFk_offset;
+    pos[5] = iAmp*fKnee_amp * sin(fAfreq*t + fPhase) + 1581 - iFk_offset;
+    pos[6] = iAmp*fKnee_amp * sin(fAfreq*t + fGait + fPhase) + 1500 + iBk_offset;
+    pos[7] = iAmp*fKnee_amp * sin(fAfreq*t + fGait + fPhase) + 1538 - iBk_offset;
     
-    //write servo positions
-    HB.writeMicroseconds(pos[2]);
-    KF.writeMicroseconds(pos[6]);
-    SB.writeMicroseconds(pos[0]); 
-    EB.writeMicroseconds(pos[4]);
-    HF.writeMicroseconds(pos[3]); 
-    KB.writeMicroseconds(pos[7]);
-    SF.writeMicroseconds(pos[1]); 
-    EF.writeMicroseconds(pos[5]);
+    Serial.println(pos[0]);
+    Serial.println(pos[1]);
+    Serial.println(pos[2]);
+    Serial.println(pos[3]);
+    Serial.println(pos[4]);
+    Serial.println(pos[5]);
+    Serial.println(pos[6]);
+    Serial.println(pos[7]);
     
-    //increment time parameter
-    t+=1;
+     
+    //Print to serial monitor to see parsed results
+    Serial.println(iAmp); 
+    Serial.println(fKnee_amp); 
+    Serial.println(fAfreq); 
+    Serial.println(fGait);
+    Serial.println(fPhase);
+    Serial.println(iF_offset);
+    Serial.println(iB_offset);  
+    Serial.println(iFk_offset);
+    Serial.println(iBk_offset);
+    
+   
+    //get time in milliseconds
+    unsigned long currentMillis = millis();
+  
+    //if the time threshold has been passed
+    if(currentMillis - previousMillis >= sine_time){
+      previousMillis = currentMillis;
+    
+      //write servo positions
+      HB.writeMicroseconds(pos[2]);
+      KF.writeMicroseconds(pos[6]);
+      SB.writeMicroseconds(pos[0]); 
+      EB.writeMicroseconds(pos[4]);
+      HF.writeMicroseconds(pos[3]); 
+      KB.writeMicroseconds(pos[7]);
+      SF.writeMicroseconds(pos[1]); 
+      EF.writeMicroseconds(pos[5]);
+    
+      //increment time parameter
+      t+=1;
+    }
   }
 }
